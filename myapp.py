68998ada6f3671f1a4696fd1aeac2037b21654c2
kbui1904 <99047685+kbui1904@users.noreply.github.com>
2022-04-20 20:48:52 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,6 +5,10 @@ from PIL import Image
 
 chart_data = pd.Dataframe(np.random.randn(20, 3), columns=['a', 'b', 'c', 'd'])
 
-image = 
+image = Image.open("Logo-KDT-JU.webp")
 st.image(image)
 st.write("My first web app")
+
+st.cache
+
+st.write(chart_data)
