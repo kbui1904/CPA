93dd110965c391ea4fe33c41f912f4bab48fa593
kbui1904 <99047685+kbui1904@users.noreply.github.com>
2022-04-20 20:44:36 +0200

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from PIL import Image
+
+chart_data = pd.Dataframe(np.random.randn(20, 3), columns=['a', 'b', 'c', 'd'])
+
+image = 
+st.image(image)
+st.write("My first web app")
